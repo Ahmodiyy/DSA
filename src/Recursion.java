21b2d05d6c8843134e2c713c76283c8e0ec253ba
Ahmodiyy <ahmodolaitan03@gmail.com>
2022-08-08 22:02:56 +0100

    welcome back to java

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,2 +1,9 @@
 public class Recursion {
+    public int getFibonacci(int fibonacciNTerm){
+        return getFibonacci(fibonacciNTerm - 1) + getFibonacci( fibonacciNTerm - 2);
+    }
+    public static void main(String[] args) {
+        Recursion recursion = new Recursion();
+        System.out.println("welcome back to java");
+    }
 }
