4a327e9513fdc7e593b8e4d06f74e9b8dab900bf
Ahmodiyy <ahmodolaitan03@gmail.com>
2022-08-08 22:10:51 +0100

    fibonacci base case

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,5 +1,9 @@
 public class Recursion {
     public int getFibonacci(int fibonacciNTerm){
+        if (fibonacciNTerm == 0 || fibonacciNTerm == 1)
+        {
+            return fibonacciNTerm;
+        }
         return getFibonacci(fibonacciNTerm - 1) + getFibonacci( fibonacciNTerm - 2);
     }
     public static void main(String[] args) {
