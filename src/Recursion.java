7ffaa35d4ed0f232dc86ac8da8a6ad1a50c76fdb
Ahmodiyy <ahmodolaitan03@gmail.com>
2022-08-09 17:01:56 +0100

    adding unintentional case to decimal to binary method

--- src/Recursion.java
+++ src/Recursion.java
@@ -10,17 +10,19 @@ public class Recursion {
         return getFibonacci(fibonacciNTerm - 1) + getFibonacci( fibonacciNTerm - 2);
     }
     public String decimalToBinary(int decimalValue){
+        if (decimalValue < 0) {
+            return "provide positive decimal value";
+        }
         if (decimalValue == 1) {
             return String.valueOf(1);
         }
          int remainder = decimalValue%2;
-
         return decimalToBinary(decimalValue/2) + remainder;
     }
     public static void main(String[] args) {
         Recursion recursion = new Recursion();
         int fibonacciNthTerm = 10;
         System.out.println("The " + fibonacciNthTerm +"th fibonacci number is : " + recursion.getFibonacci(fibonacciNthTerm));
-        System.out.println(recursion.decimalToBinary(8));
+        System.out.println(recursion.decimalToBinary(-7));
     }
 }
